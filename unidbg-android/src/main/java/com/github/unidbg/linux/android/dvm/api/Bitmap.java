2f23e73b18107fe39bf765c921fe5092f08c8528
xfxfxiaofeng <1622246366@qq.com>
2026-06-11 22:39:25 +0800

    fix(android): sync bitmap pixels on unlock

--- unidbg-android/src/main/java/com/github/unidbg/linux/android/dvm/api/Bitmap.java
+++ unidbg-android/src/main/java/com/github/unidbg/linux/android/dvm/api/Bitmap.java
@@ -7,6 +7,7 @@ import com.sun.jna.Pointer;
 
 import java.awt.image.BufferedImage;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 public class Bitmap extends DvmObject<BufferedImage> {
 
@@ -21,6 +22,27 @@ public class Bitmap extends DvmObject<BufferedImage> {
     }
 
     public void unlockPixels() {
+        if (memoryBlock != null) {
+            BufferedImage image = getValue();
+            if (image != null) {
+                int width = image.getWidth();
+                int height = image.getHeight();
+                Pointer pointer = memoryBlock.getPointer();
+                byte[] pixels = pointer.getByteArray(0, width * height * 4);
+                ByteBuffer buffer = ByteBuffer.wrap(pixels);
+                buffer.order(ByteOrder.LITTLE_ENDIAN);
+                for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                        int rgba = buffer.getInt();
+                        int a = (rgba >> 24) & 0xff;
+                        int b = (rgba >> 16) & 0xff;
+                        int g = (rgba >> 8) & 0xff;
+                        int r = rgba & 0xff;
+                        image.setRGB(x, y, (a << 24) | (r << 16) | (g << 8) | b);
+                    }
+                }
+            }
+        }
         freeMemoryBlock(null);
     }
 
